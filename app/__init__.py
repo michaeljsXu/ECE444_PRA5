566a39856ed0c4b9be35a9b43faef130afe12bd9
michaeljsXu <michaelx.0277@gmail.com>
2024-10-23 20:40:43 -0400

    added test cases, apssing

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
-import json
+
 
 application = Flask(__name__)
 
@@ -23,6 +23,18 @@ def predict(text):
 def index():
     return "Your Flask App Works! V1.0"
 
+# returns FAKE if fake
+# returns REAL if real
+@application.route("/predict", methods=['POST'])
+def predict_route():
+    try:
+        data = request.get_json(force=True)
+        text = data['text']
+        print(text)
+        prediction = predict(text)
+        return jsonify({'prediction': prediction}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
     application.run(port=5000, debug=True)
